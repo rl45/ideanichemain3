bde673ed4aedeec2f1da9301aa1c72afc4637f73
Raymond Lee <raymondlee@Raymonds-iMac.local>
2020-11-20 15:35:04 -0800

    fg

--- client/src/reportWebVitals.js
+++ client/src/reportWebVitals.js
@@ -1,6 +1,6 @@
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('../node_modules/web-vitals/dist').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
